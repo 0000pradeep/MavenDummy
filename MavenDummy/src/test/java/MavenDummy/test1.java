160cfaa4e5a63ce4783bf6efa7abd34326ffabf4
pradeep boolchandani <pradeepboolchandani81@gmail.com>
2018-07-22 16:57:53 +0530

    Committing the changes

---
+++ MavenDummy/src/test/java/MavenDummy/test1.java
@@ -0,0 +1,12 @@
+package MavenDummy;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class test1 {
+ 
+	@Test
+  public void test() {
+		System.out.println("Test method called");
+  }
+}
