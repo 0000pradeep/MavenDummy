4d0720a748711685bba2fc53a64e44a363b40748
pradeep boolchandani <pradeepboolchandani81@gmail.com>
2018-07-22 22:57:20 +0530

    removing unwanted packages

--- MavenDummy/src/test/java/MavenDummy/test1.java
+++ MavenDummy/src/test/java/MavenDummy/test1.java
@@ -2,9 +2,8 @@ package MavenDummy;
 
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-import org.testng.annotations.Test;
+
 class test4 {
 	
 	@BeforeMethod
