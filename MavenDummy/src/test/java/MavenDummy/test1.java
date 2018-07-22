4992a2498547d9be709ca87643a15ec25ab952ff
pradeep boolchandani <pradeepboolchandani81@gmail.com>
2018-07-22 17:18:28 +0530

    before test added

--- MavenDummy/src/test/java/MavenDummy/test1.java
+++ MavenDummy/src/test/java/MavenDummy/test1.java
@@ -1,10 +1,15 @@
 package MavenDummy;
 
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import org.testng.annotations.Test;
 
 public class test1 {
  
+	@BeforeTest
+	  public void beforetest() {
+			System.out.println("Before Test called");
+	  }
 	@Test
   public void test() {
 		System.out.println("Test called");
